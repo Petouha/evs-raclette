ae722bd3da6a35f4293286c6320cba9b1001fd5c
Petouha <rachidgames2.rg@gmail.com>
2025-01-11 02:29:06 +0100

    implemented ec elgamal with ecdsa

--- e_vote_system.py
+++ e_vote_system.py
@@ -276,6 +276,102 @@ def ecelgamal_dsa():
     voting_system(priv_voting_system)
     print("\n-----FIN-----\n")      
 
+def ecelgamal_ecdsa():
+    """
+    Implementation en utilisant ecelgamal pour le chiffrement et ecdsa pour la signature.
+    """
+    print("-----Implentation du vote avec EC ElGamal et ECDSA-----\n")
+    priv_voting_system,pub_voting_system = ecelgamal.ECEG_generate_keys()
+    
+    def voters_generate(pub_voting_system):
+        """
+        La fonction qui englobe les votants.
+        Paramètres:
+        pub_system_vote : clé publique du système de vote
+        Return:
+        votes : une liste des bulletins de vote composé d'un dict avec (chiffre,signature,cle_publique_votant) pour chaque votant
+        """
+        return_votes = []
+        for i in range(5):
+            # Génération des clés du votant
+            priv_voter, pub_voter = ecdsa.ECDSA_generate_keys()
+            # Génération du vote pour les 5 candidats sous forme d'une liste qui contient un seul 1 et des 0
+            generated_votes = [0]*5
+            generated_votes[dsa.randint(0, 4)] = 1
+            print(f"Les votes du votant {i} : {generated_votes}")
+            
+            # Chiffrement de chaque vote à mettre dans une liste
+            encrypted_votes = []
+            for vote in generated_votes:
+                encrypted_votes.append(ecelgamal.ECEG_encrypt(vote, pub_voting_system))
+        
+            # Signature de tous les votes, on concatène les chiffres des votes pour avoir un seul message puis on le hash et on signe
+            all_votes = ""
+            for vote in encrypted_votes:
+                u,v = vote
+                all_votes += str(u) + str(v)
+            
+            hashed_all_votes = (dsa.H(all_votes.encode()))
+            signature = ecdsa.ECDSA_sign(hashed_all_votes, priv_voter)
+            
+            # Ajout du vote chiffré, de la signature et de la clé publique du votant dans le dictionnaire puis dans la liste
+            dict_vote = {'votes': encrypted_votes, 'signature': signature, 'pub_voter': pub_voter}
+            return_votes.append(dict_vote)
+        return return_votes
+    
+    def voting_system(priv_voting_system):
+        """
+        Le système de votes qui va déchiffrer les votes et les compter.
+        Paramètres:
+        priv_system_vote : clé privée du système de vote
+        Return:
+        result : les résultats du vote
+        """
+        
+        # Génération des votes
+        voters = voters_generate(pub_voting_system)
+        # Initialisation des résultats du vote
+        results_encrypted = [((1,0),(1,0))]*len(voters)
+        results = []
+        
+        for i,votes in enumerate(voters):
+            # Vérification de la signature du votant
+            # Commencer par concaténer les votes pour les hasher
+            concat_votes = ""
+            for vote in votes['votes']:
+                u,v = vote
+                concat_votes += str(u) + str(v)
+            
+            hashed_votes = (ecdsa.H(concat_votes.encode()))
+            # Si la signature n'est pas valide, on passe au votant suivant
+            if not ecdsa.ECDSA_verify(hashed_votes, votes['signature'][0],votes['signature'][1],votes['pub_voter']):
+                print(f"La signature du votant {i} n'est pas valide")
+                continue
+            # Additionner les votes pour chaque candidat en utilisant la fonction add de ecelgamal
+            encrypted_votes = votes['votes']
+            for j,vote in enumerate(encrypted_votes):
+                (u,v) = results_encrypted[j]
+                u = ecelgamal.add(u[0],u[1],vote[0][0],vote[0][1],ecelgamal.p) #C1
+                v = ecelgamal.add(v[0],v[1],vote[1][0],vote[1][1],ecelgamal.p) #C2
+                results_encrypted[j] = (u,v)
+            
+        # Déchiffrement des votes et affichage des résultats en utilisant la fonction bruteECLog
+        for result in results_encrypted:
+            r,c = result
+            decrypted = ecelgamal.ECEG_decrypt((r,c),priv_voting_system)
+            results.append(ecelgamal.bruteECLog(decrypted[0],decrypted[1],ecelgamal.p))
+
+        print(f"Les résultats du vote sont : {results}")
+        return results
+                
+            
+  
+    
+    voting_system(priv_voting_system)
+    print("\n-----FIN-----\n")
+    
+
 elgamal_dsa()
 elgamal_ecdsa()
 ecelgamal_dsa()
+ecelgamal_ecdsa()
